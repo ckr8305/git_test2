28a6cf9cd16d28776ea1f850caf9a912b584c8ad
ckr8305 <ckr8305@naver.com>
2024-09-11 11:22:32 +0900

    Main.java 수정(오류 수정)

--- src/main/java/net/skhu/git_test2/Main.java
+++ src/main/java/net/skhu/git_test2/Main.java
@@ -2,7 +2,7 @@ package net.skhu.git_test2;
 
 public class Main {
     public static void main(String[] args) {
-        Hello hello = new Hello("hello world");
+        Hello hello = new Hello();
         System.out.println(hello.say());
     }
 }
