b34086e911ad4bdb89ec88e5f6f773fa8d1d0faf
ckr8305 <ckr8305@naver.com>
2024-09-11 11:18:29 +0900

    Merge branch 'main' of https://github.com/ckr8305/git_test2

--- src/main/java/net/skhu/git_test2/Main.java
+++ src/main/java/net/skhu/git_test2/Main.java
@@ -2,7 +2,7 @@ package net.skhu.git_test2;
 
 public class Main {
     public static void main(String[] args) {
-        Hello h = new Hello("hello world");
-        h.say();
+        Hello hello = new Hello("hello world");
+        System.out.println(hello.say());
     }
 }
